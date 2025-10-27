80cfe905225d06dcae7250eaa586a6726a20e71e
briwno <brunoalvesbezerra.ba@gmail.com>
2025-10-27 09:52:14 -0300

    desisto

--- next.config.mjs
+++ next.config.mjs
@@ -8,21 +8,6 @@ const nextConfig = {
     ],
     formats: ['image/webp'],
   },
-
- async rewrites() {
-  return [
-    {
-      source: '/',
-      has: [
-        {
-          type: 'host',
-          value: 'promo.kaaguy.app',
-        },
-      ],
-      destination: '/divulgar',
-    },
-  ];
-},
 };
 
 export default nextConfig;
